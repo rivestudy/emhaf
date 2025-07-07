5620b1019ebf6f98a647f32e7f7c2158abf8fea3
rivestudy <galang0559@gmail.com>
2025-07-07 21:40:34 +0700

    07_07_25_2
    re tailwind fix

--- App.tsx
+++ App.tsx
@@ -1,13 +1,33 @@
-import { ScreenContent } from 'components/ScreenContent';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'expo-status-bar';
 
-import './global.css';
+import HomeScreen from './screens/HomeScreen';
+import QuestScreen from './screens/QuestScreen';
+import ScanScreen from './screens/ScanScreen';
+import CompletedScreen from './screens/CompletedScreen';
+
+import './global.css'; 
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: { backgroundColor: '#1f2937' }, // Tailwind: bg-gray-800
+          tabBarLabelStyle: { color: 'white' },
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Quest" component={QuestScreen} />
+        <Tab.Screen name="Scan" component={ScanScreen} />
+        <Tab.Screen name="Completed" component={CompletedScreen} />
+      </Tab.Navigator>
       <StatusBar style="auto" />
-    </>
+    </NavigationContainer>
   );
 }
